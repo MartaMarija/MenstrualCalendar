f359d4490cca73d64a0114001f56ae362295af2d
MartaMarija <mpicic@student.foi.hr>
2023-01-05 02:06:21 +0100

    Date logic

--- mobile-app/src/pages/Calendar.tsx
+++ mobile-app/src/pages/Calendar.tsx
@@ -7,6 +7,8 @@ import {
     TouchableOpacity,
 } from 'react-native'
 import { Calendar, DateData } from 'react-native-calendars'
+import { MarkingProps } from 'react-native-calendars/src/calendar/day/marking'
+import { MarkedDates } from 'react-native-calendars/src/types'
 import OptionList from '../components/OptionList'
 
 const screen = Dimensions.get('window')
@@ -15,34 +17,95 @@ const CalendarScreen = () => {
     const [pressed, setPressed] = useState(false)
     const [dayPressed, setDayPressed] = useState<DateData>()
 
+    const b = {
+        days: [
+            {
+                date: '2023-01-01',
+                color: 'red',
+            },
+            {
+                date: '2023-01-02',
+                color: 'red',
+            },
+            {
+                date: '2023-01-24',
+                color: 'red',
+            },
+        ],
+    }
+
+    let markedDatesArray: MarkedDates = {}
+
+    for (let i = 0; i < b['days'].length; i++) {
+        markedDatesArray[b['days'][i].date] = {
+            color: b['days'][i].color,
+            textColor: 'white',
+        }
+    }
+    //console.log(markedDatesArray)
+
+    //arr = JSON.parse(b);
+
+    // `
+    //     '2023-01-11': {
+    //         startingDay: true,
+    //         color: 'red',
+    //         textColor: 'white',
+    //     },
+    //     '2023-01-13': {
+    //         endingDay: true,
+    //         color: 'red',
+    //         textColor: 'white',
+    //     },
+    //     '2023-01-12': {
+    //         color: 'red',
+    //         textColor: 'white',
+    //     },
+    //     '2023-01-24':{
+    //         startingDay: true,
+    //         endingDay: true,
+    //         color: '#F564A9',
+    //         textColor: 'white'
+    //     }
+    // `;
+
     return (
         <View>
             <Calendar
-                onDayLongPress={day => {
+                onDayPress={day => {
                     setDayPressed(day)
                     setPressed(true)
                 }}
                 markingType={'period'}
-                markedDates={{
-                    '2023-01-11': {
-                        startingDay: true,
-                        color: 'red',
-                        textColor: 'white',
-                    },
-                    '2023-01-13': {
-                        endingDay: true,
-                        color: 'red',
-                        textColor: 'white',
-                    },
-                    '2023-01-12': {
-                        color: 'red',
-                        textColor: 'white',
-                    },
-                    '2023-01-24':{
-                        startingDay: true,
-                        endingDay: true,
-                        color: '#F564A9',
-                        textColor: 'white'
+                markedDates={markedDatesArray}
+                onMonthChange={(date: DateData) => {
+                    //Moram uzeti raspon od 2 mjeseca. Trenutni i prethodni
+                    let begginigMonth = date.month - 1
+                    if (begginigMonth == 0) {
+                        let month = '12'
+                        let year = date.year - 1
+                        console.log('\nPOČETAK: ' + year + '-' + month + '-01')
+                    } else {
+                        console.log(
+                            '\nPOČETAK: ' +
+                                date.year +
+                                '-' +
+                                begginigMonth +
+                                '-01'
+                        )
+                    }
+
+                    if (date.month + 1 == 13) {
+                        let month = '01'
+                        let year = date.year + 1
+                        console.log(year + '-' + month + '-01')
+                    } else {
+                        let month = date.month + 1
+                        if (month.toString().length == 1) {
+                            console.log(date.year + '-' + 0 + month + '-01')
+                        } else {
+                            console.log(date.year + '-' + month + '-01')
+                        }
                     }
                 }}
             />
