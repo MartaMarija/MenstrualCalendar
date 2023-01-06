34b1fca7868d98cffd56fa6cbb2f171f6a964dc3
MartaMarija <mpicic@student.foi.hr>
2023-01-07 00:40:08 +0100

    Updated triggers and added more data to menstrual_cycle

--- api/src/db/migrations/1672247416337-trigger.ts
+++ api/src/db/migrations/1672247416337-trigger.ts
@@ -7,78 +7,67 @@ export class trigger1672247416337 implements MigrationInterface {
     await queryRunner.query(`set schema '${schemaName}';`);
 
     await queryRunner.query(`
-      CREATE OR REPLACE FUNCTION update_last_menstrual_cycle()
-      RETURNS TRIGGER AS $$
-      BEGIN
-        UPDATE menstrual_cycle
-        SET cycle_duration = EXTRACT(day FROM (new.cycle_start_date - cycle_start_date)),
-            ovulation_date = new.cycle_start_date - luteal_phase_duration
-        WHERE userId = new.userId
-        AND (SELECT COUNT(*) FROM menstrual_cycle WHERE userId = new.userId) > 0
-        AND cycle_start_date = (SELECT max(cycle_start_date) FROM menstrual_cycle WHERE userId = new.userId);
-      END;
-      $$ LANGUAGE plpgsql;
-
-      CREATE OR REPLACE FUNCTION update_user_avg_durations()
-      RETURNS TRIGGER AS $$
-      DECLARE new_avg_cycle_duration int;
-      DECLARE new_avg_menstruation_duration int;
-      DECLARE num_of_cycles int;
-      BEGIN
-          WITH last_7_cycles AS (
-              SELECT cycle_duration, menstruation_duration
-              FROM menstrual_cycle
-              WHERE "userId" = NEW."userId"
-              ORDER BY cycle_start_date DESC
-              LIMIT 7
-          )
-          SELECT COUNT(*) INTO num_of_cycles FROM last_7_cycles;
-          IF num_of_cycles > 0 THEN
-            SELECT AVG(cycle_duration), AVG(menstruation_duration)
-            INTO new_avg_cycle_duration,  new_avg_menstruation_duration
-            FROM last_7_cycles;
-            UPDATE "user" SET avg_duration_of_menstrual_cycle = COALESCE(new_avg_cycle_duration, avg_duration_of_menstrual_cycle), 
-            avg_duration_of_menstruation = COALESCE(new_avg_menstruation_duration, avg_duration_of_menstruation) 
-            WHERE id = NEW."userId";
-          END IF;
-          RETURN NEW;
-      END;
-      $$ LANGUAGE plpgsql;
-
-      CREATE OR REPLACE FUNCTION update_new_menstrual_cycle()
-      RETURNS TRIGGER AS $$
-      BEGIN
-        NEW.cycle_duration = (SELECT avg_cycle_duration FROM "user" WHERE id = NEW.userId);
-        NEW.menstruation_duration = (SELECT avg_menstruation_duration FROM "user" WHERE id = NEW.userId);
-        NEW.luteal_phase_duration = (SELECT avg_luteal_phase_duration FROM "user" WHERE id = NEW.userId);
-        NEW.menstruation_end_date = NEW.cycle_start_date + NEW.menstruation_duration - INTERVAL '1 day';
-        NEW.ovulation_date = NEW.cycle_start_date + NEW.cycle_duration - NEW.luteal_phase_duration - INTERVAL '1 day';
-        RETURN NEW;
-      END;
-      $$ LANGUAGE plpgsql;
-
       CREATE OR REPLACE FUNCTION update_menstrual_cycle()
       RETURNS TRIGGER AS $$
+      DECLARE last_cycle menstrual_cycle%ROWTYPE; 
+      DECLARE selected_row_id uuid;
+      DECLARE new_avg_cycle_duration int = 0;
+      DECLARE new_avg_menstruation_duration int = 0;
+      DECLARE counter int = 0;
+      DECLARE pomCD int = 0;
+      DECLARE pomMD int = 0;
       BEGIN
-        PERFORM update_last_menstrual_cycle();
-        PERFORM update_user_avg_durations();
-        PERFORM update_new_menstrual_cycle();
+        --update_last_menstrual_cycle
+        SELECT * INTO last_cycle FROM menstrual_cycle WHERE "userId" = NEW."userId" ORDER BY cycle_start_date DESC LIMIT 1;
+        
+        IF last_cycle IS NOT NULL THEN    
+            UPDATE menstrual_cycle
+            SET cycle_duration = (NEW.cycle_start_date - last_cycle.cycle_start_date),
+                ovulation_date = NEW.cycle_start_date - last_cycle.luteal_phase_duration
+            WHERE "userId" = NEW."userId"
+            AND id = last_cycle.id;
+        END IF;
+        
+        --update_user_avg_durations
+        FOR selected_row_id IN SELECT id FROM menstrual_cycle WHERE "userId" = OLD."userId" ORDER BY cycle_start_date DESC OFFSET 1 ROWS LIMIT 7 LOOP
+          counter := counter + 1;
+          SELECT cycle_duration INTO pomCD FROM menstrual_cycle WHERE id = selected_row_id;
+          SELECT menstruation_duration  INTO pomMD FROM menstrual_cycle WHERE id = selected_row_id;
+          new_avg_cycle_duration := new_avg_cycle_duration + pomCD;
+          new_avg_menstruation_duration := new_avg_menstruation_duration + pomMD;
+        END LOOP;
+        
+        IF new_avg_cycle_duration <> 0 THEN
+          new_avg_cycle_duration := new_avg_cycle_duration/counter;
+          new_avg_menstruation_duration := new_avg_menstruation_duration/counter;
+          UPDATE "user" SET avg_duration_of_menstrual_cycle = new_avg_cycle_duration, 
+          avg_duration_of_menstruation = new_avg_menstruation_duration
+          WHERE id = OLD."userId";
+        END IF;
+        
+        --update_new_menstrual_cycle
+        NEW.cycle_duration = (SELECT avg_duration_of_menstrual_cycle FROM "user" WHERE id = NEW."userId");
+        NEW.menstruation_duration = (SELECT avg_duration_of_menstruation FROM "user" WHERE id = NEW."userId");
+        NEW.luteal_phase_duration = (SELECT avg_duration_of_luteal_phase FROM "user" WHERE id = NEW."userId");
+        NEW.menstruation_end_date = NEW.cycle_start_date + NEW.menstruation_duration - INTERVAL '1 day';
+        NEW.ovulation_date = NEW.cycle_start_date + NEW.cycle_duration - NEW.luteal_phase_duration - INTERVAL '1 day';
+            
         RETURN NEW;
       END;
       $$ LANGUAGE plpgsql;
 
-      CREATE OR REPLACE TRIGGER updates_before_insert_on_menstrual_cycle_trigger
+      CREATE OR REPLACE TRIGGER update_menstrual_cycle_trigger
       BEFORE INSERT ON menstrual_cycle
       FOR EACH ROW
       EXECUTE PROCEDURE update_menstrual_cycle();
 
       CREATE OR REPLACE FUNCTION update_menstruation_duration()
       RETURNS TRIGGER AS $$
+      DECLARE difference int;
       BEGIN
-          UPDATE menstrual_cycle
-          SET menstruation_duration = (NEW.menstruation_end_date - cycle_start_date) + interval '1 day'
-          WHERE id = NEW.id;
-          RETURN NEW;
+        SELECT (NEW.menstruation_end_date - cycle_start_date + 1 ) INTO difference FROM menstrual_cycle WHERE id = NEW.id;
+        NEW.menstruation_duration = difference;
+        RETURN NEW;
       END;
       $$ LANGUAGE plpgsql;
 
@@ -86,6 +75,46 @@ export class trigger1672247416337 implements MigrationInterface {
       BEFORE UPDATE ON menstrual_cycle
       FOR EACH ROW
       EXECUTE PROCEDURE update_menstruation_duration();
+
+      CREATE OR REPLACE FUNCTION update_last_menstruation()
+      RETURNS TRIGGER AS $$
+      DECLARE selected_row_id uuid;
+      DECLARE new_avg_cycle_duration int = 0;
+      DECLARE new_avg_menstruation_duration int = 0;
+      DECLARE counter int = 0;
+      DECLARE pomCD int = 0;
+      DECLARE pomMD int = 0;
+      BEGIN
+        FOR selected_row_id IN SELECT id FROM menstrual_cycle WHERE "userId" = OLD."userId" ORDER BY cycle_start_date DESC OFFSET 1 ROWS LIMIT 7 LOOP
+          counter := counter + 1;
+          SELECT cycle_duration INTO pomCD FROM menstrual_cycle WHERE id = selected_row_id;
+          SELECT menstruation_duration  INTO pomMD FROM menstrual_cycle WHERE id = selected_row_id;
+          new_avg_cycle_duration := new_avg_cycle_duration + pomCD;
+          new_avg_menstruation_duration := new_avg_menstruation_duration + pomMD;
+        END LOOP;
+
+        IF new_avg_cycle_duration <> 0 THEN
+          new_avg_cycle_duration := new_avg_cycle_duration/counter;
+          new_avg_menstruation_duration := new_avg_menstruation_duration/counter;
+          UPDATE "user" SET avg_duration_of_menstrual_cycle = new_avg_cycle_duration, 
+          avg_duration_of_menstruation = new_avg_menstruation_duration
+          WHERE id = OLD."userId";
+
+          SELECT id into selected_row_id FROM menstrual_cycle WHERE "userId" = OLD."userId" ORDER BY cycle_start_date DESC LIMIT 1;
+          UPDATE "menstrual_cycle" SET cycle_duration = new_avg_cycle_duration, 
+          menstruation_duration = new_avg_menstruation_duration,
+          ovulation_date = cycle_start_date + new_avg_cycle_duration - luteal_phase_duration 
+          WHERE id = selected_row_id;
+        END IF;
+
+        RETURN OLD;
+      END;
+      $$ LANGUAGE plpgsql;
+
+      CREATE OR REPLACE TRIGGER update_last_menstruation_trigger
+      after DELETE ON menstrual_cycle
+      FOR EACH ROW
+      EXECUTE PROCEDURE update_last_menstruation();
 
     `);
   }
