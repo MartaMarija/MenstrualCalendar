34269ee74173133b8b49c5e651b6ef5f88ebcae2
MartaMarija <mpicic@student.foi.hr>
2023-01-07 16:51:06 +0100

    yarn pretty

--- api/src/controllers/userController.ts
+++ api/src/controllers/userController.ts
@@ -5,21 +5,21 @@ import * as userService from "../services/userService";
 import * as jwtService from "../services/jwtService";
 const router = express.Router();
 
-router.post("/login",async (req, res, next) => {
-  let user : User | null = await userService.getUserbyEmail(req.body.email);
-  if(!user){
+router.post("/login", async (req, res, next) => {
+  let user: User | null = await userService.getUserbyEmail(req.body.email);
+  if (!user) {
     return next(new AppError("Invalid email!", 400));
   }
-  if(user.password != req.body.password){
+  if (user.password != req.body.password) {
     return next(new AppError("Invalid password!", 401));
   }
-  res.json({token: jwtService.getToken(user.id)});
-})
+  res.json({ token: jwtService.getToken(user.id) });
+});
 
 //Primjer
 // router.post("/test", async (req, res, next) => {
 //   const token = req.headers['authorization']?.split(' ')[1];
-//   if(token != undefined){ 
+//   if(token != undefined){
 //     res.json({id: jwtService.authenticateToken(token)})
 //   }
 //   else{
--- api/src/services/jwtService.ts
+++ api/src/services/jwtService.ts
@@ -1,20 +1,20 @@
 import * as dotenv from "dotenv";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 dotenv.config();
 
-export interface jwtData{
-  id : string,
-  iat : number
+export interface jwtData {
+  id: string;
+  iat: number;
 }
 
 export function getToken(userId: string) {
-    return jwt.sign({id: userId}, process.env.ACCESS_TOKEN_KEY as string);
-  }
+  return jwt.sign({ id: userId }, process.env.ACCESS_TOKEN_KEY as string);
+}
 
 export function authenticateToken(token: string) {
-  try{
-    return jwt.verify(token, process.env.ACCESS_TOKEN_KEY as string) as jwtData
-  } catch{
-    return null
+  try {
+    return jwt.verify(token, process.env.ACCESS_TOKEN_KEY as string) as jwtData;
+  } catch {
+    return null;
   }
 }
